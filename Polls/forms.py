29e114d9e2c70095a02f29f194998f461e281923
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-08-23 15:03:20 -0300

    styled form and create a new button

--- Polls/forms.py
+++ Polls/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class WordForm(forms.Form):
-    your_word = forms.CharField(widget=forms.TextInput)
+    your_word = forms.CharField(widget=forms.TextInput(attrs=
+    {
+        'placeholder': 'Name', 
+         'style': 'position: relative', 
+        'class': 'form-word'
+     
+     }))
 
 class UpperForm(forms.Form):
     upper_word = forms.CharField(widget=forms.Textarea(attrs={'name': 'upper'}))
