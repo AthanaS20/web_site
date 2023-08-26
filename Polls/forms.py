1762e63078f60e3255ab7d625be44a9c48af2d3f
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-08-26 15:27:47 -0300

    update

--- Polls/forms.py
+++ Polls/forms.py
@@ -4,14 +4,14 @@ class WordForm(forms.Form):
     your_word = forms.CharField(widget=forms.TextInput(attrs=
     {
         'placeholder': 'Name', 
-         'style': 'position: relative', 
-        'class': 'form-word'
+        'style': 'position: relative', 
+        'class': 'form-word',
      
 
      }))
 
 class UpperForm(forms.Form):
-    upper_word = forms.CharField(widget=forms.Textarea(attrs={'name': 'upper'}))from django import forms
+    upper_word = forms.CharField(widget=forms.Textarea(attrs={'name': 'upper'}))
 
 class NameForm(forms.Form):
     anyword = forms.CharField(max_length=100, label="word")
